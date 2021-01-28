a32e317620457f39b4443be6b0b8fee012c262af
Colton Acosta <colto.acosta@gmail.com>
2021-01-28 09:26:43 -0700

    Edited Source Code File

---
+++ lab1/main.cpp
@@ -0,0 +1,28 @@
+//******************************************************************************
+// FILE: main.cpp
+//
+// DESCRIPTION
+// This program does x, y, and z. Then it explodes with such force that it
+// triggers massive world-wide geothermal volcanic activity that ultimately
+// results in the almost complete extinction of all life on Earth with the
+// exception of members of the order Blattodea, which have always been destined
+// to someday rule the world anyway.
+//
+// AUTHOR1: Colton Acosta, 1214326892, cacost12@asu.edu 
+//
+// COURSE INFO
+// CSE100 Principles of Programming with C++, Fall 2020
+// Lab Project 1
+// Day/Time: Thursday, 9:00 - 9:50 AM
+// TA: Neha Rajendra Vadnere
+//******************************************************************************
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout << "Hello World!" << endl;
+return 0;
+}
+
