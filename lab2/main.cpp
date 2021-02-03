d671cb096af772f031e6e848eddc481f18f4c439
Colton Acosta <colto.acosta@gmail.com>
2021-02-03 14:43:21 -0700

    Added lab2 project description

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,1 +1,30 @@
-// Lab 2 Project
+//******************************************************************************
+// FILE: main.cpp
+//
+// DESCRIPTION
+// Use include pre-processor directives to include header file such as cmath, iomanip, and istream
+// Define and use a named constant
+// Use cout to send string literals int and double values to the output window
+// Assign values using an assignment operator
+// Store integer and real numbers read from the keyboard using cin into int and double variables
+// Write arithmetic expressions using the arithmetic operators
+// Understand operator precedence rules and the difference between integer and floating point division
+// Use the statis_cast<> operator
+// Call C++ standard library math function
+// Use fixed and setprecision stream manipulators to output real numbers with a specific number of digits after the decimal point
+// Use indentation and proper spacing to format code 
+// 
+//
+// AUTHOR1: Colton Acosta, cacost12@asu.edu, 1214326892 
+//
+// COURSE INFO
+// CSE100 Principles of Programming with C++, Fall 2020
+// Lab Project 2 Day/Time: your-lab-date-and-time TA: your-lab-ta's-names
+//******************************************************************************
+//------------------------------------------------------------------------------
+// main() is where the program begins executing.
+//------------------------------------------------------------------------------
+int main(void){
+return 0;
+}
+
