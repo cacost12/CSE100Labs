2ba8fffb672d5ad5f2e605fc7b731d2640a8e043
Colton Acosta <cacost12@asu.edu>
2021-03-04 14:24:43 -0700

    Added files for lab4

---
+++ lab4/src/main.cpp
@@ -0,0 +1,143 @@
+//******************************************************************************
+// FILE: main.cpp
+//
+// DESCRIPTION
+// Reads the coefficients of quadratic equations from "coeffs.txt". Calculates
+// the roots of each equation and outputs the results to "roots.txt".
+//
+// AUTHOR1: your name, your ASURITE username, your email address
+// AUTHOR2: your name, your ASURITE username, your email address
+//
+// COURSE INFO
+// CSE100 Principles of Programming with C++, Fall 2020
+// Lab Project 4   Day/Time: your-lab-date-and-time   TA: your-lab-ta's-name
+//==============================================================================
+//                                  TESTING
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// TEST CASE 1 (This test case was given to the students)
+// Description:  Tests that the two real roots of 2.5x^2 + 7.6x - 3 = 0 are
+// calculated correctly.
+//
+// Input Data:
+// a = 2.5, b = 7.6, c = -3
+//
+// Expected Output:
+// -----------------------------------------------------------------------------
+// The equation 2.50000x^2 + 7.60000x + -3.00000 = 0 has roots 0.35361 and
+// -3.39361.
+//
+// Actual Output:
+// -----------------------------------------------------------------------------
+// copy-and-paste the contents of roots.txt here
+//
+// Test Case Result: Write PASSED or FAILED
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// TEST CASE 2:
+// Description: write a description
+//
+// Input Data:
+// a = ?, b = ?, c = ?
+//
+// Expected Output:
+// -----------------------------------------------------------------------------
+// document the expected output
+//
+// Actual Output:
+// -----------------------------------------------------------------------------
+// copy-and-paste the contents of roots.txt here
+//
+// Test Case Result: Write PASSED or FAILED
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// TEST CASE 3:
+// Description: write a description
+//
+// Input Data:
+// a = ?, b = ?, c = ?
+//
+// Expected Output:
+// -----------------------------------------------------------------------------
+// document the expected output
+//
+// Actual Output:
+// -----------------------------------------------------------------------------
+// copy-and-paste the contents of roots.txt here
+//
+// Test Case Result: Write PASSED or FAILED
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// TEST CASE 4:
+// Description: write a description
+//
+// Input Data:
+// a = ?, b = ?, c = ?
+//
+// Expected Output:
+// -----------------------------------------------------------------------------
+// document the expected output
+//
+// Actual Output:
+// -----------------------------------------------------------------------------
+// copy-and-paste the contents of roots.txt here
+//
+// Test Case Result: Write PASSED or FAILED
+//******************************************************************************
+//#include ???         // For sqrt()
+//#include ???         // For ifstream and ofstream classes
+//#include ???         // For setprecision()
+//#include ???         // For fixed and endl
+
+using namespace std;
+
+//------------------------------------------------------------------------------
+// read_coeff()
+//
+// Students: Write comments which explain what this function does. You can copy
+// the comments from the pseudocode in the Software Design.
+//
+// Important Note: Make sure to specify that the parameter 'fin' is defined as
+// type ifstream& (we will discuss what the & means later in the course so
+// don't worry about it for now).
+//------------------------------------------------------------------------------
+???
+
+//------------------------------------------------------------------------------
+// discriminant()
+//
+// Students: Write comments which explain what this function does. You can copy
+// the comments from the pseudocode in the Software Design.
+//------------------------------------------------------------------------------
+???
+
+//------------------------------------------------------------------------------
+// calc_root1()
+//
+// Students: Write comments which explain what this function does. You can copy
+// the comments from the pseudocode in the Software Design.
+//------------------------------------------------------------------------------
+???
+
+//------------------------------------------------------------------------------
+// calc_root2()
+//
+// Students: Write comments which explain what this function does. You can copy
+// the comments from the pseudocode in the Software Design.
+//------------------------------------------------------------------------------
+???
+
+//------------------------------------------------------------------------------
+// output()
+//
+// Students: Write comments which explain what this function does. You can copy
+// the comments from the pseudocode in the Software Design.
+//------------------------------------------------------------------------------
+???
+
+//------------------------------------------------------------------------------
+// main()
+//
+// Students: Write comments which explain what this function does. You can copy
+// the comments from the pseudocode in the Software Design.
+//------------------------------------------------------------------------------
+???
