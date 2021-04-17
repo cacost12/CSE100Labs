d2ad2aceea1eadc493779b740f2630caacf4079a
Colton Acosta <cacost12@asu.edu>
2021-04-17 11:20:13 -0700

    Completed get file

--- lab8/src/get.cpp
+++ lab8/src/get.cpp
@@ -1,16 +1,18 @@
-//****************************************************************************************
+//******************************************************************************
 // FILE: get.cpp
 //
 // DESCRIPTION
-// This file contains a function to display a prompt and ask the user to enter an integer.
-// The entered integer has to be within a specified range of integers, e.g., [1, 4] and
-// the function will prompting and reading integers until the user finally enters an int
-// which is within the range.
+// This file contains a function to display a prompt and ask the user to enter
+// an integer. The entered integer has to be within a specified range of 
+// integers, e.g., [1, 4] and the function will prompting and reading integers
+// until the user finally enters an int which is within the range.
+//
+// AUTHOR1: Colton Acosta, cacost12, 1214326892, cacost12@asu.edu 
 //
 // COURSE INFO
-// CSE100 Principles of Programming with C++, Spring 2021
-// Lab Project 8  Day/Time: <your-lab-date-and-time>  TA: <your-lab-ta's-name>
-//****************************************************************************************
+// CSE100 Principles of Programming with C++, Fall 2020
+// Lab Project 8   Day/Time: Th 9:00-9:50 AM TA: Neha Rajendra Vadnere
+//******************************************************************************
 #include <iostream>
 #include <string>
 #include "get.hpp"
@@ -21,9 +23,9 @@ using namespace std;
 // FUNCTION: get_double()
 //
 // DESCRIPTION
-// Display the prompt string prompt, read a double from the keyboard, and return the
-// double.
-//----------------------------------------------------------------------------------------
+// Display the prompt string prompt, read a double from the keyboard, and return
+// the double.
+//------------------------------------------------------------------------------
 double get_double(string prompt)
 {
     cout << prompt;
@@ -32,12 +34,13 @@ double get_double(string prompt)
     return n;
 }
 
-//----------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // FUNCTION: get_int()
 //
 // DESCRIPTION
-// Display the prompt string prompt, read an int from the keyboard, and return the int.
-//----------------------------------------------------------------------------------------
+// Display the prompt string prompt, read an int from the keyboard, and return 
+// the int.
+//------------------------------------------------------------------------------
 int get_int(string prompt)
 {
     cout << prompt;
@@ -46,17 +49,18 @@ int get_int(string prompt)
     return n;
 }
 
-//----------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // get_int_in()
 //
-// Displays a message prompting the user to enter an integer in the range [low, high].
-// Does not return until the user enters a value choice, which is the function return
-// value.
+// Displays a message prompting the user to enter an integer in the range [low, 
+// high]. Does not return until the user enters a value choice, which is the 
+// function return value.
 //
 // REMARKS
-// This is a sentinel loop, with the sentinel being any integer in the range [low, high].
-// Make sure that when you write the loop that you do not omit the second cin statement at
-// the bottom of the while loop. If you do, you will create an infinite loop.
+// This is a sentinel loop, with the sentinel being any integer in the range 
+// [low, high]. Make sure that when you write the loop that you do not omit the 
+// second cin statement at the bottom of the while loop. If you do, you will 
+// create an infinite loop.
 //
 // PSEUDOCODE
 // function get_int_in (msg: string, low: int, high: int) -> int
@@ -69,10 +73,25 @@ int get_int(string prompt)
 //     end while
 //     return n
 // end function
-//----------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 int get_int_in(string msg, int low, int high)
 {
-    ???
+    // Output Message
+    cout << msg;
+ 
+    // Get integer from the keyboard
+    int n;
+    cin >> n;
+
+    // Loop until the number is in the correct range
+    while (n<low && n>high){
+        cout << "Your choice must be in " << "[" << low << "-" << high "]";
+        cout << endl;
+        cout << msg;
+        cin >> n;
+    }
+
+    return(n);
 }
 
 //--------------------------------------------------------------------------------------------------
--- lab8/src/prelab.cpp
+++ lab8/src/prelab.cpp
@@ -1,9 +1,9 @@
-//****************************************************************************************
+//******************************************************************************
 // FILE: prelab.cpp
 //
 // DESCRIPTION
-// This program is designed to exercise the student's skills at writing various types of
-// loops.
+// This program is designed to exercise the student's skills at writing various 
+// types of loops.
 //
 // LEARNING OBJECTIVES
 // After completing this lab project, the student will be able to:
@@ -13,31 +13,31 @@
 // * Write a vary loop.
 // * Write a sentinel loop.
 //
-// Note: Be sure to include both AUTHOR lines if you work with a partner. If you work
-// alone, then delete the AUTHOR2 line. Be sure to include your ASURITE ID in the zip
-// archive filename and if you work with a partner, be sure to include both of your
-// ASURITE ID's.
+// Note: Be sure to include both AUTHOR lines if you work with a partner. If you
+// work alone, then delete the AUTHOR2 line. Be sure to include your ASURITE ID 
+// in the zip archive filename and if you work with a partner, be sure to 
+// include both of your ASURITE ID's.
 //
 // AUTHOR1: Colton Acosta, cacost12, 1214326892, cacost12@asu.edu 
 //
 // COURSE INFO
 // CSE100 Principles of Programming with C++, Fall 2020
 // Lab Project 8   Day/Time: Th 9:00-9:50 AM TA: Neha Rajendra Vadnere
-//****************************************************************************************
+//******************************************************************************
 
 #include <iostream>
 
 // Write #include directives to include get.hpp and prelab.hpp
-???
-???
+#include "get.hpp"
+#include "prelab.hpp"
 
 using namespace std;
 
-//----------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // NAMED CONSTANTS
 //
 // These constants correspond to the menu item numbers displayed in menu().
-//----------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 static const int MENU_FIRST      = 1;
 static const int MENU_VARY_FOR1  = 1;
 static const int MENU_VARY_FOR2  = 2;
@@ -45,34 +45,47 @@ static const int MENU_VARY_WHILE = 3;
 static const int MENU_RETURN     = 4;
 static const int MENU_LAST       = 4;
 
-//----------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // FUNCTION PROTOTYPES
 //
-// Write prototypes for any function which is defined below the location in this source
-// code file from where the function is first called.
+// Write prototypes for any function which is defined below the location in this
+// source code file from where the function is first called.
 //----------------------------------------------------------------------------------------
 ???
 
 //----------------------------------------------------------------------------------------
 // menu()
 //
-// This function is called from run() to display the menu for the program. The user will
-// select one of these menu items to perform. Note that get_int_in() will not return until
-// the user enters an integer in [1, 4] so it is guaranteed that the returned value will
-// be a valid choice.
+// This function is called from run() to display the menu for the program. The u
+// ser will select one of these menu items to perform. Note that get_int_in() 
+// will not return until the user enters an integer in [1, 4] so it is 
+// guaranteed that the returned value will be a valid choice.
 //
 // PSEUDOCODE
 // function menu() -> int
 //     output a blank line
 //     output each of the menu item lines
-//     return what call get_int_in("Your Choice 1-4]? ", MENU_FIRST, MENU_LAST) returns
+//     return what call get_int_in("Your Choice 1-4]? ", MENU_FIRST, MENU_LAST)
+// returns
 // end function
-//----------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 static int menu()
 {
-    // Implement the pseudocode to implement the Prelab Exercises Menu as shown on p. 4
-    // of the lab document.
-    ???
+    // Implement the pseudocode to implement the Prelab Exercises Menu as shown 
+    // on p.4 of the lab document.
+    int choice;
+
+    cout << endl;
+    cout << "-------------------------------------------" << endl;
+    cout << "Lab 8 Main Menu: What would you like to do?" << endl;
+    cout << "-------------------------------------------" << endl;
+    cout << "1. Run the prelab exercizes." << endl;
+    cout << "2. Compute the a-th root of a real number." << endl;
+    cout << "3. Quit the program." << endl;
+    cout << "Your Choice [1-3]?";
+    cin >> choice; 
+
+    return(choice);
 }
 
 //----------------------------------------------------------------------------------------
@@ -102,7 +115,8 @@ static void process_menu_choice(int choice)
 //----------------------------------------------------------------------------------------
 // vary_for1()
 //
-// See the Prelab Exercises section of the project document for the software requirements.
+// See the Prelab Exercises section of the project document for the software 
+// requirements.
 //
 // PSEUDOCODE
 // function vary_for1()
@@ -115,19 +129,26 @@ static void process_menu_choice(int choice)
 // end function
 //
 // REMARKS
-// Note that in this loop, every integer is followed by one space including the last
+// Note that in this loop, every integer is followed by one space including the 
+// last
 // integer.
-//----------------------------------------------------------------------------------------
-static void vary_for1()
-{
+//------------------------------------------------------------------------------
+static void vary_for1(){
     // Implement the pseudocode.
-    ???
+    cout << "[";
+
+    for (int i = -1; i >= -25; --i){
+        cout << i << " ";
+    }
+   
+    cout << "]" << endl << endl;
 }
 
-//----------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // vary_for2()
 //
-// See the Prelab Exercises section of the project document for the software requirements.
+// See the Prelab Exercises section of the project document for the software 
+// requirements.
 //
 // PSEUDOCODE
 // function vary_for2()
@@ -152,19 +173,26 @@ static void vary_for1()
 // end function
 //
 // REMARKS
-// Note that in this loop, every integer is followed by one space except for the last
-// integer, which is not followed by a space.
-//----------------------------------------------------------------------------------------
-static void vary_for2()
-{
+// Note that in this loop, every integer is followed by one space except for the
+// last integer, which is not followed by a space.
+//------------------------------------------------------------------------------
+static void vary_for2(){
+
     // Implement either the first or the alternative version of the pseudocode.
-    ???
+    cout << "[";
+    
+    for (int i=-1; i>-25; --i){
+        cout << i << " ";
+    }
+
+    cout << -25 << "]"<<endl<<endl;
 }
 
-//----------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // vary_while()
 //
-// See the Prelab Exercises section of the project document for the software requirements.
+// See the Prelab Exercises section of the project document for the software 
+// requirements.
 //
 // PSEUDOCODE
 // function vary_while()
@@ -177,19 +205,28 @@ static void vary_for2()
 //     output ']'
 //     output blank line
 // end function
-//----------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 static void vary_while()
 {
     // Implement the pseudocode.
-    ???
+    cout << "[";
+
+    int n = 100;
+
+    while(n>=1){
+        cout << n << " ";
+        n -= 3;
+    }
+
+    cout << "]" << endl << endl;
 }
 
-//----------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // run_prelab()
 //
-// This function implements a sentinel loop which will repeatedly display the menu, get
-// the user's choice, call the appropriate looping function, and then the sentinel loop
-// repeats.
+// This function implements a sentinel loop which will repeatedly display the 
+// menu, get the user's choice, call the appropriate looping function, and then
+// the sentinel loop repeats.
 //
 // PSEUDOCODE
 // function run() -> nothing
@@ -200,7 +237,7 @@ static void vary_while()
 //     end while
 //     output a blank liine
 // end function
-//----------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 void run_prelab()
 {
     // Implement the pseudocode
